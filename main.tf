bf832f9db8e8546cc264b5f9bf07a884e91d3107
Priya <priaysu@gmail.com>
2024-02-07 14:56:55 -0600

    expense terraform started

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source       = "./modules/vpc"
   for_each     = var.vpc
-  vpc_cidr     = lookup(each.value,"vpc_cidr",null)
+  vpc_cidr     = each.value["vpc_cidr"]
   subnets_cidr = lookup(each.value,"subnets_cidr",null)
   az           = lookup(each.value,"az",null)
 
