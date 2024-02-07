3dec260dd8ec313c3af0a6a35eb4896590ee36a8
Priya <priaysu@gmail.com>
2024-02-07 17:39:37 -0600

    expense terraform started

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block = "var.vpc_cidr"
+  #cidr_block = "var.vpc_cidr"
+  cidr_block = "10.10.0.0/21"
 
   tags = {
     Name = "${var.env}-${var.project_name}-vpc"
