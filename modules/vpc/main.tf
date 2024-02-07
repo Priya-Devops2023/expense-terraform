a61c4a07ea8845c02b759c0c9869963dd14f6036
Priya <priaysu@gmail.com>
2024-02-07 14:19:47 -0600

    expense terraform started

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-${var.project_name}-vpc"
   }
 }
+
+resource "aws_subnet" "main" {
+  count = length(var.subnets_cidr)
+  vpc_id =  aws_vpc.main.id
+  cidr_block = element(var.subnets_cidr,count.index )
+  availability_zone = element(var.az,count.index)
+
+  tags = {
+     Name = "subnet-${count.index}"
+  }
+}
